b8a4d82812f26837f85d876f6965ce5c9e701e73
Debaswinee Samanta <samantadebaswinee999@gmail.com>
2025-12-02 15:40:10 +0530

    Add simple interest calculation script

---
+++ simple-interest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Simple Interest Calculation
+P=1000  # Principal Amount
+R=5     # Rate of Interest (in %)
+T=2     # Time (in years)
+
+# Calculate Simple Interest (SI)
+SI=$(( (P * R * T) / 100 ))
+
+echo "The calculated Simple Interest is: $SI"
+#!/bin/bash
+# Simple Interest Calculation
+P=1000  # Principal Amount
+R=5     # Rate of Interest (in %)
+T=2     # Time (in years)
+
+# Calculate Simple Interest (SI)
+SI=$(( (P * R * T) / 100 ))
+
+echo "The calculated Simple Interest is: $SI"
